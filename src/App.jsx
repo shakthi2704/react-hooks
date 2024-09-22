fb09c59bc1223ed74b33e62aa00b5cf208b9747d
Satheesh <59468802+shakthi2704@users.noreply.github.com>
2024-09-22 12:38:23 +0530

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import State from "./hooks/state"
+import StateTwo from "./hooks/StateTwo"
 
 const App = () => {
   return (
     <div>
       <State />
+      <StateTwo />
     </div>
   )
 }
